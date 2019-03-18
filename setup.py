470d221f7331ed86f1d3144937d7ee07956f8f38
AlaBouali <trap.leader.123@gmail.com>
2019-03-18 04:33:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bane",
-    version="1.3.4",
+    version="1.3.5",
     author="AlaBouali",
     author_email="trap.leader.123@gmail.com",
     description="cyber security library",
