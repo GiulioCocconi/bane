c0daed81e5972857b2541dfa30aad56aaa996e73
AlaBouali <trap.leader.123@gmail.com>
2019-03-18 21:13:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bane",
-    version="1.4.2",
+    version="1.4.3",
     author="AlaBouali",
     author_email="trap.leader.123@gmail.com",
     package_data={'bane':'bane'},
