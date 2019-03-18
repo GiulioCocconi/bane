4e112f09f151407fcce5db15bd878ab7a6c7244d
AlaBouali <trap.leader.123@gmail.com>
2019-03-18 23:20:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,15 @@ import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-requirements=['requests','PySocks','bs4','paramiko','pexpect','mysql-connector','scapy','stem']
-
 setuptools.setup(
     name="bane",
-    version="1.4.9",
+    version="1.5.0",
     author="AlaBouali",
     author_email="trap.leader.123@gmail.com",
     description="cyber security library",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/AlaBouali/bane",
-    install_requires=requirements,
     python_requires=">=2.7",
     license="MIT License",
     classifiers=[
