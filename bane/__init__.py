4c5e80814ec0d6458f0c16eeb29d5ba73fba47e1
AlaBouali <trap.leader.123@gmail.com>
2019-03-19 20:01:54 +0100

    Update __init__.py

--- bane/__init__.py
+++ bane/__init__.py
@@ -13,61 +13,18 @@ linkedin:
 
       https://www.linkedin.com/in/ala-bouali-bb17ab132/
 '''
-import cgi,HTMLParser,httplib,os,sys,urllib,socket,random,time,hashlib,base64,threading,ssl,urllib2,re,smtplib,telnetlib,subprocess
+import cgi,HTMLParser,httplib,requests,socks,pexpect,os,sys,urllib,socket,random,time,hashlib,base64,threading,ssl,urllib2,re,smtplib,telnetlib,subprocess
 from struct import *
 from ftplib import FTP
-'''
-   checking for dependencies and installing the missing ones
-'''
-try:
-  import pexpect
-  from pexpect import pxssh
-except:
-  os.system('pip install pexpect')
-  import pexpect
-try:
- import paramiko
- from paramiko import SSHClient, AutoAddPolicy
-except:
-  os.system('pip install paramiko')
-  import paramiko
-  from paramiko import SSHClient, AutoAddPolicy
-try:
-  import socks
-except:
-   os.system('pip install PySocks')
-   import socks
-try:
-  import requests
-except:
- os.system('pip install requests')
- import requests
-try:
-  import bs4
-  from bs4 import BeautifulSoup
-except:
-  os.system('pip install bs4')
-  import bs4
-  from bs4 import BeautifulSoup
-try:
-  from scapy.all import *
-except:
-  os.system('pip install scapy')
-  from scapy.all import *
-try:
-  import stem
-  from stem import Signal
-  from stem.control import Controller
-except:
-  os.system('pip install stem')
-  import stem
-  from stem import Signal
-  from stem.control import Controller
-try:
- import mysql.connector as mconn
-except:
-  os.system('pip install mysql-connector')
-  import mysql.connector as mconn
+import paramiko
+from paramiko import SSHClient, AutoAddPolicy
+import bs4
+from bs4 import BeautifulSoup
+from scapy.all import *
+import stem
+from stem import Signal
+from stem.control import Controller
+import mysql.connector as mconn
 from payloads import *
 def md5(w):
  '''
