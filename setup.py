c3c4dc40289446dd3c7cefc512a597c488b9ed92
AlaBouali <trap.leader.123@gmail.com>
2019-03-19 02:25:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/AlaBouali/bane",
     python_requires=">=2.7",
-    install_requires=["requests","PySocks","bs4","paramiko","pexpect","mysql-connector","scapy","stem"]
+    install_requires=["requests","PySocks","bs4","paramiko","pexpect","mysql-connector","scapy","stem"],
     packages=["bane"],
     license="MIT License",
     classifiers=[
