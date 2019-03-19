54939cfc0a61ca5f9a3c00d6d59cf99d95cd91b5
AlaBouali <trap.leader.123@gmail.com>
2019-03-19 01:04:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name="bane",
-    version="1.5.1",
+    version="1.5.2",
     author="AlaBouali",
     author_email="trap.leader.123@gmail.com",
     description="cyber security library",
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/AlaBouali/bane",
     python_requires=">=2.7",
+    packages=["bane"],
     package_dir={'bane': 'bane'},
     license="MIT License",
     classifiers=[
