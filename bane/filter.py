cb0c79ec1577bc9f51ac52518560305fd911d001
AlaBouali <trap.leader.123@gmail.com>
2019-03-23 19:54:03 +0100

    Update filter.py

--- bane/filter.py
+++ bane/filter.py
@@ -1,8 +1,10 @@
+from extrafun import escape_html,unescape_html
+from bs4 import BeautifulSoup
 '''
 the following functions use a strict rules to make sure that the returned input is 100% safe
 '''
 
-def xss(s,tags=['br','h1','h2','h3','h4','h5','h6','p','strong','pre','b','center']):
+def filter_xss(s,tags=['br','h1','h2','h3','h4','h5','h6','p','strong','pre','b','center']):
  '''
  the xss vulnerability is a major security problem. There are a couple of methods to prevent it,one of them is by escape all user's input.
  that would work with search boxes or normal inputs, but sometimes it's necessery to add some HTML tags in the input and here it comes the problem!!!
@@ -30,7 +32,7 @@ def xss(s,tags=['br','h1','h2','h3','h4','h5','h6','p','strong','pre','b','cente
    s=s.replace(y,z)
  return s
 
-def html(s,unescape=True):
+def filter_html(s,unescape=True):
  '''
  this function is to remove all html tags
  '''
@@ -38,7 +40,7 @@ def html(s,unescape=True):
   s=unescape_html(s)
  return BeautifulSoup(s, "lxml").text
 
-def injections(s):
+def filter_injections(s):
  '''
  in any injection attack (sql injection, code injection, command injection...), the attacker always add in his input ";" then his malicious injection.
  so the best act to sanitize the input is to remove everything after the ";".
@@ -67,5 +69,3 @@ def filter_fi(s,ext='php',remote=None):
     return None
   return None
  return s
- 
- 
