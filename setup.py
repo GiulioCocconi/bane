aee8c6b5774921d89f5e08f38a1170ad76c23ebf
AlaBouali <trap.leader.123@gmail.com>
2019-05-05 18:24:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ req=["requests","PySocks","bs4","pexpect","paramiko","mysql-connector","scapy","
 
 setuptools.setup(
     name="bane",
-    version="1.9.2",
+    version="1.9.3",
     author="AlaBouali",
     author_email="trap.leader.123@gmail.com",
     description="cyber security library",
