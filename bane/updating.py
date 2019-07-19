a2b7b8c509c5924558f9c2e2e6c462fa0235e1ca
AlaBouali <trap.leader.123@gmail.com>
2019-07-19 01:45:45 +0100

    Update updating.py

--- bane/updating.py
+++ bane/updating.py
@@ -1,14 +1,27 @@
 import os
-def updater(version=None):
+def update_py2(version=None):
  if os.path.isdir('/home/')==True:
   os.system('sudo pip uninstall bane -y')
   if version:
-   os.system('sudo pip install bane')
-  else:
    os.system('sudo pip install bane=='+str(version))
+  else:
+   os.system('sudo pip install bane')
  else:
   os.system('pip uninstall bane -y')
   if version:
-   os.system('pip install bane')
+   os.system('pip install bane=='+str(version))
   else:
-   os.system('sudo pip install bane=='+str(version))
+   os.system('pip install bane)
+def update_py3(version=None):
+ if os.path.isdir('/home/')==True:
+  os.system('sudo pip3 uninstall bane -y')
+  if version:
+   os.system('sudo pip3 install bane=='+str(version))
+  else:
+   os.system('sudo pip3 install bane')
+ else:
+  os.system('pip3 uninstall bane -y')
+  if version:
+   os.system('pip3 install bane=='+str(version))
+  else:
+   os.system('pip3 install bane)
