7fc685c057db0c6366cd553182a1bfaf89c17776
AlaBouali <trap.leader.123@gmail.com>
2020-04-05 07:28:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ if termux==True:
  os.system('pkg install nodejs -y')
 setuptools.setup(
     name="bane",
-    version="3.9.7",
+    version="3.9.8",
     author="AlaBouali",
     author_email="trap.leader.123@gmail.com",
     description="cyber security library",
